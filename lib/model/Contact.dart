6ef4ac1caecf2d984a9956bd5fe13f9c55206d3f
MARILIA CAROLINA CAMPANINE MACEDO <39626109+MariliaCampanine@users.noreply.github.com>
2020-08-23 22:06:52 -0300

    add list view on emergency screen

---
+++ lib/model/Contact.dart
@@ -0,0 +1,11 @@
+
+class Contact {
+  final String name;
+  final String phone;
+
+  Contact({
+    this.name,
+    this.phone
+  });
+
+}
--- lib/screens/emergency/emergency_page.dart
+++ lib/screens/emergency/emergency_page.dart
@@ -5,62 +5,7 @@ import 'package:flutter/rendering.dart';
 import 'package:chameleon/screens/home/widgets/buttom_custom.dart';
 //imports packages
 import 'package:google_fonts/google_fonts.dart';
-
-showAlertDialog2(BuildContext context) {
-
-  final _formKey = GlobalKey<FormState>();
-  final nameController = TextEditingController();
-  final phoneController = TextEditingController();
-
-  Widget cancelaButton = FlatButton(
-    child: Text("Cancelar"),
-    onPressed: Navigator.of(context).pop ,
-  );
-  Widget continuaButton = FlatButton(
-    child: Text("Continar"),
-    onPressed:  () {},
-  );
-  //configura o AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: Text("Adicionar novo contato"),
-    content: Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.name,
-            controller: nameController,
-            decoration: InputDecoration(hintText: "Nome"),
-            validator: (value) {
-              if (value.isEmpty) return 'Digite o nome';
-              return null;
-            }
-          ),
-          TextFormField(
-            keyboardType: TextInputType.phone,
-            controller: phoneController,
-            decoration: InputDecoration(hintText: "Telefone"),
-            validator: (value) {
-              if (value.isEmpty) return 'Digite o número';
-              return null;
-            }
-          )
-        ],
-      ),
-    ),
-    actions: [
-      cancelaButton,
-      continuaButton,
-    ],
-  );
-  //exibe o diálogo
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
-}
+import 'package:chameleon/model/Contact.dart';
 
 class EmergencyPage extends StatefulWidget {
   @override
@@ -68,6 +13,83 @@ class EmergencyPage extends StatefulWidget {
 }
 
 class _EmergencyPageState extends State<EmergencyPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  List list = List<Contact>();
+
+  addContact(Contact contact) {
+    this.list.add(contact);
+    cleanDialog();
+  }
+
+  cleanDialog() {
+    nameController.text = "";
+    phoneController.text = "";
+  }
+
+  showAlertDialog2(BuildContext context) {
+
+    Widget cancelaButton = FlatButton(
+      child: Text("Cancelar"),
+      onPressed: Navigator.of(context).pop ,
+    );
+    Widget continuaButton = FlatButton(
+      child: Text("Continar"),
+      onPressed:  () {
+        addContact(new Contact(name: nameController.text, phone: phoneController.text));
+        Navigator.pop(context);
+      },
+    );
+    //configura o AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Adicionar novo contato"),
+      content: Form(
+
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+                keyboardType: TextInputType.name,
+                controller: nameController,
+                decoration: InputDecoration(hintText: "Nome"),
+                validator: (value) {
+                  if (value.isEmpty) return 'Digite o nome';
+                  return null;
+                }
+            ),
+            TextFormField(
+                keyboardType: TextInputType.phone,
+                controller: phoneController,
+                decoration: InputDecoration(hintText: "Telefone"),
+                validator: (value) {
+                  if (value.isEmpty) return 'Digite o número';
+                  return null;
+                }
+            )
+          ],
+        ),
+      ),
+      actions: [
+        cancelaButton,
+        continuaButton,
+      ],
+    );
+    //exibe o diálogo
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 0,
+          width: 0,
+          child: alert,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -78,56 +100,48 @@ class _EmergencyPageState extends State<EmergencyPage> {
         centerTitle: true,
         backgroundColor: Color(0xFFCFA181),
 
-        leading:   new IconButton(
+        leading: new IconButton(
 
           icon: new Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(null),
         ),
       ),
-      body: Center(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         child: Column(
           children: [
-            SizedBox(height: 48),
-
-            ButtomCustom(
-                text: 'Polícia Federal',
-                textColor: Colors.white,
-                backgroundColor: Color(0xFFCFA181),
-                icon: Icons.person,
-                onClick: () {print("texto");}
-
-            ),
-            ButtomCustom(
-                text: 'Delegacia da Mulher',
-                textColor: Colors.white,
-                backgroundColor: Color(0xFFCFA181),
-                icon: Icons.person,
-                onClick: () {print("texto");}
-
-            ),
-            ButtomCustom(
-                text: 'Samu',
-                textColor: Colors.white,
-                backgroundColor: Color(0xFFCFA181),
-                icon: Icons.person,
-                onClick: () {print("texto");}
-
+            ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+              itemCount: this.list.length,
+              itemBuilder: (BuildContext context, index){
+                return ListTile(
+                  title: ButtomCustom(
+                      text: this.list[index].name,
+                      textColor: Colors.white,
+                      backgroundColor: Color(0xFFCFA181),
+                      icon: Icons.person,
+                      onClick: () {print("texto");}
+                  ),
+                );
+              }
             ),
             ButtomCustom(
                 text: 'Novo contato',
                 textColor: Colors.white,
                 backgroundColor: Color(0xFF9D7254),
                 icon: Icons.person_add,
-                onClick: showAlertDialog2(context)
-
-            ),
-
-            //fim botao mais contatos
+                onClick: (){showAlertDialog2(context);}
+            )
           ],
         ),
-      ),
+      )
     );
   }
 }
+
+
+
 
 
